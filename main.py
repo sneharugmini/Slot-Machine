5f3d921a5f6441659d687fe925de560ebb2f64fb
sneharugmini <sneharugminimv@gmail.com>
2025-02-06 19:03:00 -0500

    lines check

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
@@ -12,8 +14,57 @@ symbol_count = {
     "D": 8
 }
 
-def get_machine_spin():
-    
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winnings_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+            else:
+                winnings += values[symbol] * bet
+                winning_lines.append(line + 1)
+        return winnings, winning_lines
+
+
+def get_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end= " | ")
+            else:
+                print(column[row], end= "")
+
+        print()
+        
 
 def deposit():
     while True:
@@ -70,5 +121,11 @@ def main():
             break
 
     print(f"You are betting ${bet} for {lines} lines. Total amount is {total_bet}")
+
+    slots =  get_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"won ${winnings}")
+    print(f"you won on lines:", *winning_lines)
 
 main()
